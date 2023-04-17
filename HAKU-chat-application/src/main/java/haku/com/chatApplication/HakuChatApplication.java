b318b239f30cebe0a0b2efa718f953fe32d9d662
Alex Trueman <alex.a.trueman@aol.com>
2023-04-17 12:42:59 +0100

    first commit

---
+++ HAKU-chat-application/src/main/java/haku/com/chatApplication/HakuChatApplication.java
@@ -0,0 +1,13 @@
+package haku.com.chatApplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HakuChatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HakuChatApplication.class, args);
+	}
+
+}
